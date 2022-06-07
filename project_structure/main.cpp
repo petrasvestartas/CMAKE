68cfa3edbf65b1a64d69b91fe1e97cb90679a9db
Petras Vestartas <petrasvestartas@gmail.com>
2022-06-07 13:34:05 +0200

    project_structure

---
+++ project_structure/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "sort.hpp"
+#include "print.hpp"
+
+int main(int argc, char **argv)
+{
+  std::vector<double> example = {4, 5.4, 9.1, 1, -2.2};
+  std::cout << "Before:\n" ;
+  myPrint(example);
+  
+  mySort(example);  
+  
+  std::cout << "\nAfter:\n";
+  myPrint(example);
+  return 0;
+}
